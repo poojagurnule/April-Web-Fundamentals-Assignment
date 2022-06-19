3781837d4bdbbb3b13158e9ce0283be57a7f1b99
Your NamePooja <you@example.com>
2022-06-19 20:18:19 +0530

    test done

--- June/week3/19junetest/Q1.js
+++ June/week3/19junetest/Q1.js
@@ -1,5 +1,14 @@
- const input = [1,-5,2, 10,-30,29,50]
- const Output = input.filter(function(e){
-    return e > 1 ;
- })
- console.log(Output);
+ 
+// 1.An array is given, return the sum of all the positives numbers. const input = [ 1, -5, 2, 10, -30, 29, 50];
+        // ans 
+        const input = [ 1, -5, 2, 10, -30, 29, 50];
+        let sum=0;
+        function addition(){
+            for(let i=0;i<=input.length-1;i++){
+                if(input[i]>=0){
+                    sum=sum+input[i];
+                }
+            }
+            return sum;
+        }
+        console.log(addition(input));
